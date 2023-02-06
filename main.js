5a76645e1ac1f2a2704a070b6dd41ebf391baaa2
skunkwoks <124541693+skunkwoks@users.noreply.github.com>
2023-02-06 02:35:27 +0000

    test module

---
+++ main.js
@@ -0,0 +1,10 @@
+import { MDCRipple } from '@material/ripple';
+
+export const name = "square";
+
+export function draw(ctx, length, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, length, length);
+
+    return { length, x, y, color };
+}
